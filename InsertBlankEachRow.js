910a5c35bb8803c265910e3abc3852d45149dfcf
g-iki <g.iki.0329@gmail.com>
2016-09-08 22:47:59 +0900

    bugfix

--- InsertBlankEachRow.js
+++ InsertBlankEachRow.js
@@ -6,27 +6,27 @@ var editor   = Editor;
 switch(Editor.IsTextSelected()) {
     // 非選択状態
     case 0:
-        //editor.SelectAll();
-    break;
+        editor.SelectAll();
     // 選択状態
     case 1:
         select = editor.GetSelectedString(0);
 
         var lines = select.split(/\r\n|\r|\n/);
         var n = ['\r\n', '\r', '\n'][editor.GetLineCode()];
-        
+
+        select = lines.join(n+n);
+
     break;
     // ブロック選択状態
     case 2:
 
     break;
-    
     default:
         /* ignore */
     break;
 }
-editor.InsText(select);
 
+editor.InsText(select);
 
 editor   = null;
 window   = null;
